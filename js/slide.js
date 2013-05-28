f39a1e581d6d92469ac87dc85c6d1dbb67666dd2
yifanwu <yifan1030@gmail.com>
2013-05-28 11:20:21 -0400

    some sliding stuff

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,13 @@
-setInterval(function(){
-    $("#slide1").slideDown();
-},3000);
+
+$(document).ready(function(){
+    
+	for (var i = 0; i < 4; i++) {
+		var img = document.getElementById('slide');
+		img.src = "img/slide" + i + ".png";
+		setInterval(function(){
+			$("#slide").slideDown();
+		},1000);		
+		setInterval(function(){},1000)
+		$("#slide").hide()
+	}
+});
